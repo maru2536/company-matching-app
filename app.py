cbb6325140b461b9ba66375c12250e57a7ed5af4
maru2536 <ismr.1123@gmail.com>
2025-08-03 19:59:58 +0900

    Update app.py
    背景をホワイトに変更

--- app.py
+++ app.py
@@ -613,7 +613,7 @@ custom_css = """
 /* ベース設定 */
 body {
   font-family: 'Noto Sans JP', -apple-system, BlinkMacSystemFont, sans-serif;
-  background: white !important;
+  background: #ffffff !important;
   min-height: 100vh;
   margin: 0;
   padding: 0;
